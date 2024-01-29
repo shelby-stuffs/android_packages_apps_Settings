6f6c3496edceb331068ae877284f03333bd11548
Vala Zadeh <quic_vzadeh@quicinc.com>
2024-01-29 10:05:11 -0800

    Fix copyright marking
    Change-Id: Ia84c19c20e75a16327df118b3a9f7d22fa5870e8
    CRs-Fixed: 3692125

--- src/com/android/settings/network/telephony/CiwlanNotificationReceiver.java
+++ src/com/android/settings/network/telephony/CiwlanNotificationReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-/* Changes from Qualcomm Innovation Center are provided under the following license:
+/* Changes from Qualcomm Innovation Center, Inc. are provided under the following license:
  * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
