cfc56f1e622584b8e326c81174126facacbc83fc
hoffc <quic_hoffc@quicinc.com>
2024-03-25 11:25:24 +0800

    Settings: Adapt domestic roaming FR for AOSP change.
    NetworkOperatorPreference class has been converted from java to kotlin, need do some adaptation for the AOSP code change.
    
    Change-Id: I546321bc7728adde6866d51fab161b5aae1dcea0
    CRs-Fixed: 3768399

--- src/com/android/settings/network/telephony/NetworkSelectSettings.java
+++ src/com/android/settings/network/telephony/NetworkSelectSettings.java
@@ -593,7 +593,9 @@ public class NetworkSelectSettings extends DashboardFragment implements
                 mForbiddenPlmns, mShow4GForLTE,
                 MobileNetworkUtils.getAccessMode(getPrefContext().getApplicationContext(),
                         mTelephonyManager.getSlotIndex()));
-        preference.updateCell(cellInfo);
+        if (!DomesticRoamUtils.isFeatureEnabled(getPrefContext())) {
+            preference.updateCell(cellInfo);
+        }
         return preference;
     }
 
